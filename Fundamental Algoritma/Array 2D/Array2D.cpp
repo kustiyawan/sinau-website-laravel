8401eb0718d962f767c468d4b66c02a444622564
Hadi Kustiyawan <kustiyawan7pati@gmail.com>
2025-09-25 14:21:04 +0700

    sudah masuk ke array 2D

---
+++ Fundamental Algoritma/Array 2D/Array2D.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main () {
+	int matrix[3][3];
+	
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++){
+			scanf("%d", &matrix[i][j]);
+		}
+	}
+	
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++){
+			printf("%d ", matrix[i][j]);
+		}
+		printf("\n");
+	}
+	
+	
+	return 0;
+}
--- Fundamental Algoritma/Array/challenge.cpp
+++ Fundamental Algoritma/Array/challenge.cpp
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include<string.h>
+#include<string.h> // yang dipake di CARA KETIGA, dari librarynya
 
 int main (){
 //	mencoba sebuah contoh latihan
@@ -21,12 +21,29 @@ int main (){
 		/// agar tidak ada karakter lain selain huruf yang bakal dimasukkan
 //	scanf("%[^\n]", str_kedua);
 
-	int panjang = strlen(str);
+	int panjang = strlen(str); // untuk mengetahui jumlah karakter yang diinputkan oleh user yang dimasukkan pada variabel str
 	
+//	for (int i = 0; i < panjang; i++) {
+//		if(str[i] >= 97 && str[i] <= 122) { /// metode yang digunakan untuk mengubah huruf kecil ke besar melalui konsep ASCII,
+//			str[i] -= 32;					/// ASCII simpelnya model standar cara komputer dalam menerjemahkan karakter yang diinput pada keyboard menggunakan angka
+											/// jadi a kalau dalam kode ASCII itu 97, a sampai z itu 92 sampai 122
+//		}									/// nah didalam huruf besar yaitu A sampai Z, diawali angka 65 sampai 90
+//	}										/// jadi logikanya a = 97, A = 65, versi huruf besarnya kudu dikurangi 32, 
+											/// mangkanya ketika karakter semua yang dicek dari variabel str kode ascii antara 95 sampai 122, 
+											/// kode ASCII nya akan dikurang 32 agar menenumkan versi huruf besarnya melalui kode ASCII yang dikurangi tersebut
+											
+	/// CARA KEDUA
 	for (int i = 0; i < panjang; i++) {
-		
+		if(str[i] >= 'a' && str[i] <= 'z') { /// bisa mengggunakan abjad nya itu sendiri, program tetep akan bisa mengenalnya sebagai kode ASCII
+			str[i] -= 32;					/// jadi baris coding berikutnya masih sama dikurangi 32
+		}
 	}
 	
-
+//	printf("%s", str);
+	/// CARA KETIGA
+//	printf("%s", strupr(str)); // Menggunakan library dari string.h, strupr untuk menjadikan huruf kapital semua
+	printf("%s", strlwr(str)); // strlwr untuk mengubahnya ke huruf kecil semua
 	
+
+	return 0;
 }
