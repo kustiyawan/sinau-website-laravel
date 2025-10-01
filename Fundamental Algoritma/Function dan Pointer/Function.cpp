33da7b69326e36acb6af534fc14c36717fcc0afb
Hadi Kustiyawan <kustiyawan7pati@gmail.com>
2025-10-01 14:24:50 +0700

    masuk function dan pointer

---
+++ Fundamental Algoritma/Function dan Pointer/Function.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+	// pembuatan fungsi seperti ini
+	void printHelloWorld() {
+		printf("HELLO WORLD!!");
+	}
+
+int main(){
+	
+	// untuk memanggilnya tinggal ketik nama fungsinya
+	printHelloWorld();
+	
+	
+	return 0;
+}
