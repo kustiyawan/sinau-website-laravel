b0190ae83a1ef406ee7f489dec4a0699191d0b94
Hadi Kustiyawan <kustiyawan7pati@gmail.com>
2025-10-03 14:24:30 +0700

    function, passing by value, passing by reference

--- Fundamental Algoritma/Function dan Pointer/Function.cpp
+++ Fundamental Algoritma/Function dan Pointer/Function.cpp
@@ -4,12 +4,42 @@
 	void printHelloWorld() {
 		printf("HELLO WORLD!!");
 	}
+	
+	// gak harus void, bisa jadi menggunakan int, yang fungsinya juga untuk mengembalikkan sebuah angka
+	// hampir seperti variabel yang berinteger biasa yang bisa mengembalikkan data berupa angka,
+	// bedanya ada pengolahan/pengoperasian dulu disini
+	int getNumber() {
+		return 5;
+	}
+	
+	// function ada 1 tipe, passing by value dan passing by reference
+	
+	// PASSING BY VALUE
+	// ambil contoh dengan 2 paramater, parameter disini ialah int first dan int secon
+	int sum (int first, int second) {
+		int total = first + second; /// int first -> first
+		return total;				/// int second -> second
+								// ditotal di int total
+								// return total -> hasil penjumlahan akan dikembalikan ke int hasilSum
+	}
 
 int main(){
 	
 	// untuk memanggilnya tinggal ketik nama fungsinya
-	printHelloWorld();
+//	printHelloWorld();
+
+	int a = getNumber();
 	
+	printf("%d\n", a);
+	
+	// langsung dengan mencantumkan nama fungsi nya seperti ini juga bisa
+	printf("%d\n", getNumber());
+	
+	printf("\n");
+	/// PASSING BY VALUE
+	int hasilSum = sum(4, 8); // angka no 4 -> int first
+							  // angka no 8 -> int second
+	printf("%d\n", hasilSum);
 	
 	return 0;
 }
