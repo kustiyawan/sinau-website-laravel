a5ab4bbf666f14a262f3529cc8237006341acd5a
Hadi Kustiyawan <kustiyawan7pati@gmail.com>
2025-10-25 12:25:12 +0700

    fibonacci

--- Fundamental Algoritma/Fibonacci/Recursive.cpp
+++ Fundamental Algoritma/Fibonacci/Recursive.cpp
@@ -27,6 +27,83 @@ void angkaUrutKebalik(int i) {
 	printf("%d\n", i);
 	
 	/// kalau untuk ini menciptakan urutan 4 ke 0, terbalik
+	// gambarannya seperti ini
+//	- angkaUrutKebalik(0)
+//		perkondisian tidak memenuhi
+//		angkaUrutKebalik(0 + 1)
+//	
+//		- angkaUrutKebalik(1)
+//			perkondisian tidak memenuhi
+//			angkaUrutKebalik(1+1)
+//	
+//			- angkaUrutKebalik(2)
+//				perkondisian tidak memenuhi
+//				angkaUrutKebalik(2+1)
+//				
+//				- angkaUrutKebalik(3)
+//					perkondisian tidak memenuhi
+//					angkaUrutKebalik(3+1)
+//					
+//					- angkaUrutKebalik(4)
+//						perkondisian tidak memenuhi
+//						angkaUrutKebalik(4+1)
+//						
+//						- angkaUrutKebalik(5)
+//							perkondisian memenuhi i == 5
+//							langsung direturn kosongan
+//						- angkaUrutKebalik(4+1) / (5) ---> 0 -> ini lebih ke kosong, tidak menghasilkan angka apapun termasuk 0
+//	
+//					- angkaUrutKebalik(4)
+//						perkondisian tidak memenuhi
+//						angkaUrutKebalik(4+1)/(5) ---> 0
+//						printf("%d\n", i); ---> 4 -> i nya masih ngambil i yang ada didalam function induk yaitu angkaUrutKebalik(4)
+//						
+//					- angkaUrutKebalik(4) ---> 4
+//	
+//				- angkaUrutKebalik(3)
+//					perkondisian tidak memenuhi
+//					angkaUrutKebalik(3+1)/(4) ----> 4
+//					printf("%d\n", i); ----> 3
+//	
+//				- angkaUrutKebalik(3) ----> 4
+//										--> 3
+//	
+//			- angkaUrutKebalik(2)
+//				perkondisian tidak memenuhi
+//				angkaUrutKebalik(2+1)/(3) ---> 4
+//										-----> 3
+//				printf("%d\n", i); ---> 2
+//			
+//			- angkaUrutKebalik(2) ---> 4
+//									-> 3
+//									-> 2
+//	
+//		- angkaUrutKebalik(1)
+//			perkondisian tidak memenuhi
+//			angkaUrutKebalik(1+1)/(2) ----> 4
+//									------> 3
+//									------> 2
+//			printf("%d\n", i); ----> 1
+//		
+//		- angkaUrutKebalik(1) ----> 4
+//								--> 3
+//								--> 2
+//								--> 1
+//	
+//	- angkaUrutKebalik(0)
+//		perkondisian tidak memenuhi
+//		angkaUrutKebalik(0+1)/(1) ----> 4
+//								------> 3
+//								------> 2
+//								------> 1
+//		printf("%d\n", i); ----> 0
+//	
+//	- angkaUrutKebalik(0) ----> 4
+//							--> 3
+//							--> 2
+//							--> 1
+//							--> 0
+				
 }
 
 
