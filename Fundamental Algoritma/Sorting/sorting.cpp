935eb61629ac12d27f08d3bb6a6d40a589fab508
Hadi Kustiyawan <kustiyawan7pati@gmail.com>
2025-11-20 15:24:55 +0700

    sorting bubble sort

--- Fundamental Algoritma/Sorting/sorting.cpp
+++ Fundamental Algoritma/Sorting/sorting.cpp
@@ -25,6 +25,17 @@ int main() {
 	// pertama
 	// mencoba ascending dengan bubble sort
 	
+	// KONSEP BUBBLE SORT
+	// AMBIL CONTOH DENGAN MENGURUTKAN DARI KECIL KE BESAR
+	// bubble sort bahasa sederhananya, mengecek bilangan sebelumnya (dikiri) dengan bilangan selanjutnya (dikanan), jikalau bilangan sebelumnya
+		// lebih besar dengan bilangan selanjutnya, bilangan sebelumnya akan dipindah kekanan, bilangan selanjutnya yang tadi dikanan dipindah juga kekiri
+		// --> 			29			10			14			37			14
+		// -->			[29]  ->	10			14			37			14
+		// -->		mengecek dulu dibilangan pertama, dan terdeteksi bilangan kiri lebih besar
+		
+		// -->			[10]  ->	29			14			37			14
+		// -->			10			[29]		14			37			14
+		// -->		angka 29 dipindah kekanan, angka 10 dipindah kekiri, pengecekan berlanjut ke bilangan kedua yang sudah berubah jadi 29
 	bubbleSort();
 	print();
 	
