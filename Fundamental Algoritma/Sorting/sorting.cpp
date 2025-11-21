4718590f684162b91e3a8f9d7d921862934c8562
Hadi Kustiyawan <kustiyawan7pati@gmail.com>
2025-11-21 10:59:31 +0700

    menjelaskan cara kerja bubble sort

--- Fundamental Algoritma/Sorting/sorting.cpp
+++ Fundamental Algoritma/Sorting/sorting.cpp
@@ -36,6 +36,25 @@ int main() {
 		// -->			[10]  ->	29			14			37			14
 		// -->			10			[29]		14			37			14
 		// -->		angka 29 dipindah kekanan, angka 10 dipindah kekiri, pengecekan berlanjut ke bilangan kedua yang sudah berubah jadi 29
+		
+		// -->			10			[29]	->	14			37			14
+		// -->			10			[14]		29			37			14
+		// -->			10			14			[29]		37			14
+		// -->		langkahnya sama seperti saat merubah bilangan 10, bilangan yang kiri yang ditutup kurung terindikasi lebih besar ketimbang yang dikanan
+		// -->		jadi angka 29 juga dipindah ke kanan angka 14 dipindah kekiri, pemeriksaan berlanjut ke bilangan ketiga, yang sama sama masih dipegang 29
+		
+		// -->			10			14			[29]		37			14
+		// -->			10			14			[29]	->	37			14
+		// -->			10			14			29			[37]		14
+		// -->		ketika bilangan yang kiri (didalam kurung) lebih kecil yang ada didalam kanan, perubahan tidak akan terjadi, dan pemeriksaan akan berlanjut
+		// -->		menuju ke bilangan keempat yaitu bilangan 37
+		
+		// -->			10			14			29			[37]		14
+		// -->			10			14			29			[37]	->	14
+		// -->			10			14			29			[14]		37
+		// -->			10			14			29			14			[37]
+		// -->		masih sama langkahnya seperti biasa, setelah pada bilangan terakhir, akan diulang kembali dari awal bilangan, melakukan langkah sebelumnya
+		// -->		sampai terurut dari kecil kebesar
 	bubbleSort();
 	print();
 	
